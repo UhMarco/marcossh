7fc1e9df2e38314e15021006e9b5ac8de3edb66d
UhMarco <ddefygravity@gmail.com>
2020-10-25 22:40:41 +0000

    added some spacing because i can and also fixed a stupid

--- cmds/list.js
+++ cmds/list.js
@@ -3,13 +3,14 @@ const jsonfile = require('jsonfile');
 module.exports = (margs) => {
   args = margs._.slice(1);
 
-  const profiles = jsonfile.readFileSync('profiles.json');
+  let profiles = jsonfile.readFileSync(__dirname + '/../profiles.json');
   if (Object.values(profiles) == 0) {
-    return console.log('No profiles saved.');
+    return console.log('\n    No profiles saved.\n');
   }
-  console.log('--- Profiles: ---');
+  console.log('\n   --- Profiles: ---');
   Object.keys(profiles).forEach((key, i) => {
-    console.log(`${key}: ${profiles[key]}`);
+    console.log(`   ${key}: ${profiles[key]}`);
   });
+  console.log();
 
 };
--- cmds/remove.js
+++ cmds/remove.js
@@ -3,17 +3,17 @@ const jsonfile = require('jsonfile');
 module.exports = (margs) => {
   args = margs._.slice(1);
 
-  let profiles = jsonfile.readFileSync('profiles.json');
+  let profiles = jsonfile.readFileSync(__dirname + '/../profiles.json');
   const deleteByHostname = margs.host || margs.hostname || null
 
   if (!profiles[args[0]] && !deleteByHostname) {
-    return console.log(`Profile '${args[0]}' does not exist.`);
+    return console.log(`\n    Profile '${args[0]}' does not exist.\n`);
   } else if (deleteByHostname && Object.values(profiles).indexOf(deleteByHostname)) {
-    return console.log(`No profile is using the hostname ${deleteByHostname}.`);
+    return console.log(`\n    No profile is using the hostname ${deleteByHostname}.\n`);
   }
 
   let target = deleteByHostname ? Object.keys(profiles).find(key => profiles[key] === deleteByHostname) : args[0]
-  console.log(`Deleted profile '${target}' with hostname '${profiles[target]}.'`);
+  console.log(`\n   Deleted profile '${target}' with hostname '${profiles[target]}.'\n`);
   delete profiles[target]
-  jsonfile.writeFileSync('profiles.json', profiles);
+  jsonfile.writeFileSync(__dirname + '/../profiles.json', profiles);
 };
